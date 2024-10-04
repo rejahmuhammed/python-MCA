ad96eb6dfaf9a0db2fbd19bb818aa9b3f0709e7d
rejahmuhammed <rejahmuhammed@gmail.com>
2024-10-04 11:41:26 +0530

    Update poslist.py

--- poslist.py
+++ poslist.py
@@ -1,4 +1,6 @@
-mylist=[12,-1,1,-4,-5,7,10]
+mylist=[12,-1,1,-4,-5,7,10,0,44,-18,89]
+print("mylist:",mylist)
+print("positive integer numbers are:")
 for num in mylist:
     if num>=0:
-        print([num])
+        print([num],end=" ")
