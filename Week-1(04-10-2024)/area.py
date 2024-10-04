94718cad302db4fab5d4b5df82af8816790def8e
rejahmuhammed <rejahmuhammed@gmail.com>
2024-10-04 11:54:38 +0530

    Add files via upload

---
+++ Week-1(04-10-2024)/area.py
@@ -0,0 +1,15 @@
+r=int(input("enter the radious : "))
+print(r)
+ar=3.14*r*r
+print("area of circle")
+print(ar)
+h=int(input("enter the trayangle height : "))
+b=int(input("enter the trayangle base : "))
+abh=0.5*h*b
+print("area of trayangle")
+print(abh)
+l=int(input("enter the rectangle lenght : "))
+w=int(input("enter therectangle width : "))
+art=l*w
+print("area of rectangle")
+print(art)
---
+++ Week-1(04-10-2024)/avg.py
@@ -0,0 +1,12 @@
+n1=int(input("enter your maths mark: "))
+n2=int(input("enter your pysics mark: "))
+n3=int(input("enter your cs mark: "))
+n4=int(input("enter your chemistry mark: "))
+n5=int(input("enter your malayalam mark: "))
+
+total=n1+n2+n3+n4+n5
+print("total mark: ")
+print(total)
+avg=total/5
+print("average mark : ")
+print(avg)
