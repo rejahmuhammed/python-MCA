ce486c872bd4fbc1818755b71460ef0c76073a28
rejahmuhammed <rejahmuhammed@gmail.com>
2024-11-15 16:32:21 +0530

    Add files via upload

---
+++ week-7(15-11-24)/Calling.py
@@ -0,0 +1,6 @@
+from MyPackages import factorial
+from MyPackages import sqrt
+x=factorial.factorial(5)
+y=sqrt.square_root(64)
+print("The Factorial is ",x)
+print("The square root is ",y)
---
+++ week-7(15-11-24)/sqrt.py
@@ -0,0 +1,8 @@
+
+
+import math
+
+def square_root(x):
+    if x < 0:
+        return "Invalid"
+    return math.sqrt(x)
