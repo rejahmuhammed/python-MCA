ec2f43ca66f52bedaa269a1e14eeca489b9442bb
rejahmuhammed <rejahmuhammed@gmail.com>
2024-12-18 14:41:54 +0530

    Add files via upload

---
+++ week-8/Math_operations/Geometric_operations/area.py
@@ -0,0 +1,9 @@
+def rectangle_area(length,width):
+    return length*width
+
+def circle_area(radius):
+    from math import pi
+    return pi * (radius*radius)
+
+def triangle_area(base,height):
+    return 0.5*base*height
